5a545510e9254c25bc07475868c550969815593b
vladimir Takov <vladimir.takov@gmail.com>
2019-08-27 22:41:59 +0300

    Setup and chapter 35

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://127.0.0.1:27017';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
