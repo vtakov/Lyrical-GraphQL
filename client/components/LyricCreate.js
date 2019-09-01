d13b8e8596aecab0e3d3dec0793f5c8347c2fa5b
vladimir Takov <vladimir.takov@gmail.com>
2019-09-01 21:46:07 +0300

    Chapter 62

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import addLyricToSong from '../queries/addLyricToSong.js';
 
 class LyricCreate extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { content: '' };
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.onSubmit.bind(this)}>
                 <label>Add a lyric</label>
-                <input/>
+                <input
+                    value={this.state.content}
+                    onChange={event => this.setState({ content: event.target.value })}
+                />
             </form>
         );
     }
