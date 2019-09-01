3ad91a5204223b7b884277408e621208db161359
vladimir Takov <vladimir.takov@gmail.com>
2019-09-01 14:46:17 +0300

    Chapter 50

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
+import fetchSongs from '../queries/fetchSongs.js'
 
 class SongList extends Component {
     renderSongs() {
@@ -32,13 +32,4 @@ class SongList extends Component {
     }
 }
 
-const query = gql`
-    {
-        songs {
-            id
-            title
-        }
-    }
-`;
-
-export default graphql(query)(SongList);
+export default graphql(fetchSongs)(SongList);
