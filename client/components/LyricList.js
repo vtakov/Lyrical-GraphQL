440762f0ec43410f1d7d3c12b5631e57edc06ee6
vladimir Takov <vladimir.takov@gmail.com>
2019-09-01 22:33:44 +0300

    Chapter 67

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
 class LyricList extends Component {
+    onLike(id) {
+
+    }
+
     renderLyrics() {
         return this.props.lyrics.map(lyric => {
             return (
                 <li key={lyric.id} className="collection-item">
                     {lyric.content}
+                    <i
+                        className="material-icons"
+                        onClick={() => this.onLike(lyric.id)}
+                    >thumb_up</i>
                 </li>
             );
         });
