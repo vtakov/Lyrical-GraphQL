98c566bcece24de1ecd54741a2a940dd49329a43
vladimir Takov <vladimir.takov@gmail.com>
2019-09-01 12:55:17 +0300

    Chapter 43

--- client/index.js
+++ client/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+
+import App from './components/App.js';
 import SongList from './components/SongList.js';
 
 const client = new ApolloClient({});
@@ -9,7 +12,11 @@ const client = new ApolloClient({});
 const Root = () => {
   return (
       <ApolloProvider client={client}>
-        <SongList></SongList>
+          <Router history={hashHistory}>
+              <Route path="/" component={App}>
+                  <IndexRoute component={SongList}/>
+              </Route>
+          </Router>
       </ApolloProvider>
   );
 };
